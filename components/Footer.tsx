fd78ec7a9d8d2d15418f587da05a25af2dba1d50
Yan Zhabko <113374407+yanzhabko@users.noreply.github.com>
2024-08-23 16:45:49 +0300

    add: init structure

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface FooterProps {}
+
+const Footer: FC<FooterProps> = () => {
+  return <div></div>;
+};
+
+export default Footer;
---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface HeaderProps {}
+
+const Header: FC<HeaderProps> = () => {
+  return <div></div>;
+};
+
+export default Header;
